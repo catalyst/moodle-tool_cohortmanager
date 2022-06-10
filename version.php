3ce4fcc91a15e28a01330afb80499e84e49aac4e
Dmitrii Metelkin <dmitriim@catalyst-au.net>
2022-06-10 15:58:39 +1000

    Add github actions

--- version.php
+++ version.php
@@ -25,7 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_cohortmanager';
-$plugin->release = '0.1.0';
+$plugin->release = 2022061000;
 $plugin->version = 2022061000;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_STABLE;
+$plugin->supported = [39, 401];  // Available as of Moodle 3.9.0 or later.
