ea541194e1b55f9396d9946dbbcab13ef7d0aaaa
Dmitrii Metelkin <dmitriim@catalyst-au.net>
2022-06-14 12:15:50 +1000

    Add persistent classes for DB tables

---
+++ classes/condition.php
@@ -0,0 +1,54 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace tool_cohortmanager;
+
+use \core\persistent;
+
+/**
+ * Conditions persistent class.
+ *
+ * @package    tool_cohortmanager
+ * @author     Dmitrii Metelkin <dmitriim@catalyst-au.net>
+ * @copyright  2022 Catalyst IT
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class condition extends persistent {
+
+    /** @var string table. */
+    const TABLE = 'tool_cohortmanager_cond';
+
+    /**
+     * Return the definition of the properties of this model.
+     *
+     * @return array
+     */
+    protected static function define_properties() {
+        return [
+            'ruleid' => [
+                'type' => PARAM_INT,
+            ],
+            'classname' => [
+                'type' => PARAM_TEXT,
+            ],
+            'configdata' => [
+                'type' => PARAM_RAW,
+                'default' => '{}',
+            ],
+        ];
+    }
+
+}
---
+++ classes/rule.php
@@ -0,0 +1,92 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace tool_cohortmanager;
+
+use \core\persistent;
+
+/**
+ * Rules persistent class.
+ *
+ * @package    tool_cohortmanager
+ * @author     Dmitrii Metelkin <dmitriim@catalyst-au.net>
+ * @copyright  2022 Catalyst IT
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class rule extends persistent {
+
+    /** @var string table. */
+    const TABLE = 'tool_cohortmanager';
+
+    /**
+     * Return the definition of the properties of this model.
+     *
+     * @return array
+     */
+    protected static function define_properties() {
+        return [
+            'name' => [
+                'type' => PARAM_TEXT,
+            ],
+            'enabled' => [
+                'type' => PARAM_INT,
+                'default' => 0,
+            ],
+            'archived' => [
+                'type' => PARAM_INT,
+                'default' => 0,
+            ],
+            'cohortid' => [
+                'type' => PARAM_INT,
+                'default' => 0,
+            ],
+        ];
+    }
+
+    /**
+     * Return if the rule is enabled.
+     *
+     * @return bool
+     */
+    public function is_enabled() : bool {
+        return (bool)$this->get('enabled');
+    }
+
+    /**
+     * Return if the rule is archived.
+     *
+     * @return bool
+     */
+    public function is_archived() : bool {
+        return (bool)$this->get('archived');
+    }
+
+    /**
+     * Hook to execute after a delete.
+     *
+     * @param bool $result Whether or not the delete was successful.
+     * @return void
+     */
+    protected function after_delete($result): void {
+        global $DB;
+
+        if ($result) {
+            $DB->delete_records(condition::TABLE, ['ruleid' => $this->get('id')]);
+            $DB->delete_records(match::TABLE, ['ruleid' => $this->get('id')]);
+        }
+    }
+
+}
