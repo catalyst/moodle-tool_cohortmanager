a1544ec7cf6247bffc4d0c80cc4a18d29e91b306
Dmitrii Metelkin <dmitriim@catalyst-au.net>
2022-06-20 17:14:24 +1000

    Let other plugins implement conditions

---
+++ classes/helper.php
@@ -0,0 +1,48 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace tool_cohortmanager;
+
+/**
+ * Helper class.
+ *
+ * @package    tool_cohortmanager
+ * @author     Dmitrii Metelkin <dmitriim@catalyst-au.net>
+ * @copyright  2022 Catalyst IT
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class helper {
+
+    /**
+     * Get a list of all conditions.
+     * @return array
+     */
+    public static function get_all_conditions(): array {
+        $instances = [];
+
+        $classes = \core_component::get_component_classes_in_namespace(null, '\\tool_cohortmanager\\condition');
+
+        foreach (array_keys($classes) as $class) {
+            $reflectionclass = new \ReflectionClass($class);
+            if (!$reflectionclass->isAbstract()) {
+                $instances[] = $class::get_instance();
+            }
+        }
+
+        return $instances;
+    }
+
+}
