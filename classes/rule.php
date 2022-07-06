d293d7b1950ff2a3b99367e83453cda09bee6f9a
Dmitrii Metelkin <dmitriim@catalyst-au.net>
2022-07-06 12:55:03 +1000

    Add cache for rule condition records

--- classes/rule.php
+++ classes/rule.php
@@ -18,6 +18,7 @@ namespace tool_cohortmanager;
 
 use core\persistent;
 use cache_helper;
+use cache;
 
 /**
  * Rules persistent class.
@@ -73,9 +74,16 @@ class rule extends persistent {
      * @return condition[]
      */
     public function get_condition_records(): array {
-        $conditions = [];
-        foreach (condition::get_records(['ruleid' => $this->get('id')], 'position') as $condition) {
-            $conditions[$condition->get('id')] = $condition;
+        $cache = cache::make('tool_cohortmanager', 'rules');
+        $key = 'condition-records-' . $this->get('id');
+        $conditions = $cache->get($key);
+
+        if ($conditions === false) {
+            $conditions = [];
+            foreach (condition::get_records(['ruleid' => $this->get('id')], 'position') as $condition) {
+                $conditions[$condition->get('id')] = $condition;
+            }
+            $cache->set($key, $conditions);
         }
 
         return $conditions;
